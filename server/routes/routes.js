5a36631b5ddfe19cccf6d6bacf81224c5c49cd58
Federico Zamora <federicozamora616@gmail.com>
2024-09-15 20:04:29 -0300

    se agrego la libreria multar y el endpoint para cargar imagenes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,12 +1,27 @@
 const express = require("express")
-
+const multer = require("multer")
+const path = require("path")
 
 const router = express.Router()
 
+const diskstorage = multer.diskStorage({
+    destination: path.join(__dirname,"../images"),
+    filename:(req,file,cb) =>{
+        cb(null, Date.now() + "-" + file.originalname)
+    }
+})
 
+const fileUpload = multer({
+    storage:diskstorage
+}).single("image")
 
 router.use('/', (req,res)=>{
     res.send("gholi")
+})
+
+router.post('/images/post', fileUpload ,(req,res)=>{
+    console.log(req.file)
+
 })
 
 module.exports = router
